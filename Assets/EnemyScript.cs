6a900b8947ea5e5d918f649c9edf8f77c3fdde09
MCreator280 <johnsmacancela@gmail.com>
2026-04-19 23:23:58 -0400

    Update1
    Its the start of it

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public SpriteRenderer SR;
+    public Rigidbody2D RB;
+
+    //Enemy Stats
+    public float Speed = 5;
+    public float JumpPower = 10;
+    public float Gravity = 13;
+
+    //Enemy States
+    public bool PlayerControl = false;
+    public bool OnGround = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerControl == true)
+        {   
+            //Assigns Vel to be LinearVelocity
+            Vector2 vel = RB.linearVelocity;
+            
+            //Movement
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                vel.x = Speed;
+                Debug.Log("Right");
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                vel.x = -Speed;
+                Debug.Log("Left");
+            }
+            else
+            {
+                vel.x = 0;
+            }
+
+            //Updates LinearVelocity to match what Vel is equal at the moment
+            RB.linearVelocity = vel;
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ghost")
+        {
+            PlayerControl = true;
+            SR.color = Color.blue;
+            Destroy(other.gameObject);
+        }
+
+
+
+
+
+    }
+
+
+}
---
+++ Assets/GhostScript.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class GhostScript : MonoBehaviour
+{
+
+    public SpriteRenderer SR;
+    public Rigidbody2D RB;
+
+    //Ghost Stats
+    public float Speed = 5;
+    public float JumpPower = 10;
+    public float Gravity = 13;
+
+    //Ghost States
+    public bool PlayerControl = true;
+    public bool OnGround = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RB.gravityScale = Gravity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerControl == true)
+        {   
+            //Assigns Vel to be LinearVelocity
+            Vector2 vel = RB.linearVelocity;
+            
+            //Movement
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                vel.x = Speed;
+                Debug.Log("Right");
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                vel.x = -Speed;
+                Debug.Log("Left");
+            }
+            else
+            {
+                vel.x = 0;
+            }
+
+            //Updates LinearVelocity to match what Vel is equal at the moment
+            RB.linearVelocity = vel;
+
+            
+
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            PlayerControl = false;
+        }
+
+
+    }
+}
